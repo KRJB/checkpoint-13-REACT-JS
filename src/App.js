1abfb0f2e4c0be224ee0c45cc25d42143203fc69
KRJB <khalilrejeb0@gmail.com>
2022-07-05 18:13:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form,Button,Navbar,Container,Nav} from 'react-bootstrap';
+import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div > 
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Form App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div id="form">
+      <img src="https://raw.githubusercontent.com/briancodex/react-form-v1/286f4a4603bda257ae001dc57c74d7f30bd4eedb/public/img/img-2.svg" alt="img"/>
+      <Form>
+      <Form.Group className="mb-3">
+        <Form.Label class='name'>User name</Form.Label>
+        <Form.Control  placeholder="User name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" >
+        <Form.Label class='name'>Email address</Form.Label>
+        <Form.Control  placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" >
+        <Form.Label class='name'>Password</Form.Label>
+        <Form.Control  placeholder="Password" />
+      </Form.Group>
+      
+      
+
+      <Form.Group className="mb-3" >
+        <Form.Label class='name'>Confirm Password</Form.Label>
+        <Form.Control  placeholder="Confirm Password" />
+      </Form.Group>
+
+      <Button id='butt' variant="primary" type="submit">
+        sign up
+      </Button>
+    </Form>
+    </div>
     </div>
   );
 }
